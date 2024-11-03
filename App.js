257ab4f3d0c3e5b613ffcc821fa412daaae1cbbb
Thyerri Mezzari <thyerrimezzari@hotmail.com>
2024-11-03 17:51:30 -0300

    prepare base app

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import {
@@ -10,77 +9,70 @@ import {
   Text,
   MD3LightTheme as DefaultTheme,
 } from "react-native-paper";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import * as Location from "expo-location";
-import { getAllLocations, insertLocation } from "./db";
 import myColors from "./assets/colors.json";
 import myColorsDark from "./assets/colorsDark.json";
 
-const themeDark = {
-  ...DefaultTheme,
-  // Specify custom property
-  myOwnProperty: true,
-  // Specify custom property in nested object
-  colors: myColorsDark.colors,
-};
-
 export default function App() {
-  const [isSwitchOn, setIsSwitchOn] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [locations, setLocations] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  const [isSwitchOn, setIsSwitchOn] = useState(false); // variável para controle do darkMode
+  const [isLoading, setIsLoading] = useState(false); // variável para controle do loading do button
+  const [locations, setLocations] = useState(null); // variável para armazenar as localizações
 
-  // Tema do RN PAPER
+  // Carrega tema default da lib RN PAPER com customização das cores. Para customizar o tema, veja:
   // https://callstack.github.io/react-native-paper/docs/guides/theming/#creating-dynamic-theme-colors
   const [theme, setTheme] = useState({
     ...DefaultTheme,
-    // Specify custom property
     myOwnProperty: true,
-    // Specify custom property in nested object
     colors: myColors.colors,
   });
 
-  // darkMode
-  async function loadDarkMode() {
-    const darkMode = await AsyncStorage.getItem("@darkMode");
-    setIsSwitchOn(darkMode == "1" ? true : false);
-  }
+  // load darkMode from AsyncStorage
+  async function loadDarkMode() {}
 
+  // darkMode switch event
   async function onToggleSwitch() {
-    await AsyncStorage.setItem("@darkMode", !isSwitchOn ? "1" : "0");
     setIsSwitchOn(!isSwitchOn);
   }
 
-  // location
+  // get location (bottao capturar localização)
   async function getLocation() {
     setIsLoading(true);
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== "granted") {
-      setErrorMsg("Permission to access location was denied");
-      return;
-    }
 
-    let location = await Location.getCurrentPositionAsync({});
-    console.log(location);
-    const ret = await insertLocation(location.coords);
-    console.log(ret.lastInsertRowId);
-    await loadLocations();
+    // Localização fake, substituir por localização real do dispositivo
+    const coords = {
+      latitude: -23.5505199,
+      longitude: -46.6333094,
+    };
+
     setIsLoading(false);
   }
 
+  // load locations from db sqlite - faz a leitura das localizações salvas no banco de dados
   async function loadLocations() {
     setIsLoading(true);
-    const locations = await getAllLocations();
+
+    // generate fake locations
+    const locations = [];
+    for (let i = 0; i < 5; i++) {
+      locations.push({
+        id: i,
+        latitude: -23.5505199 + i,
+        longitude: -46.6333094 + i,
+      });
+    }
+
     setLocations(locations);
     setIsLoading(false);
   }
 
+  // Use Effect para carregar o darkMode e as localizações salvas no banco de dados
+  // É executado apenas uma vez, quando o componente é montado
   useEffect(() => {
     loadDarkMode();
     loadLocations();
   }, []);
 
-  // Efetiva a alteração do tema dark/light
+  // Efetiva a alteração do tema dark/light quando a variável isSwitchOn é alterada
+  // É executado sempre que a variável isSwitchOn é alterada
   useEffect(() => {
     if (isSwitchOn) {
       setTheme({ ...theme, colors: myColorsDark.colors });
@@ -92,32 +84,34 @@ export default function App() {
   return (
     <PaperProvider theme={theme}>
       <Appbar.Header>
-        <Appbar.Content title="Persistência RN" />
+        <Appbar.Content title="My Location BASE" />
       </Appbar.Header>
-      <View style={styles.containerDarkMode}>
-        <Text>Dark Mode</Text>
-        <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
-      </View>
-      <Button
-        style={styles.containerButton}
-        icon="map"
-        mode="contained"
-        loading={isLoading}
-        onPress={() => getLocation()}
-      >
-        Capturar localização
-      </Button>
+      <View style={{ backgroundColor: theme.colors.background }}>
+        <View style={styles.containerDarkMode}>
+          <Text>Dark Mode</Text>
+          <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+        </View>
+        <Button
+          style={styles.containerButton}
+          icon="map"
+          mode="contained"
+          loading={isLoading}
+          onPress={() => getLocation()}
+        >
+          Capturar localização
+        </Button>
 
-      <FlatList
-        style={styles.containerList}
-        data={locations}
-        renderItem={({ item }) => (
-          <List.Item
-            title="localização"
-            description={`Latitude: ${item.latitude} | Longitude: ${item.longitude}`}
-          ></List.Item>
-        )}
-      ></FlatList>
+        <FlatList
+          style={styles.containerList}
+          data={locations}
+          renderItem={({ item }) => (
+            <List.Item
+              title={`Localização ${item.id}`}
+              description={`Latitude: ${item.latitude} | Longitude: ${item.longitude}`}
+            ></List.Item>
+          )}
+        ></FlatList>
+      </View>
     </PaperProvider>
   );
 }
@@ -140,5 +134,6 @@ const styles = StyleSheet.create({
   },
   containerList: {
     margin: 10,
+    height: "100%",
   },
 });
